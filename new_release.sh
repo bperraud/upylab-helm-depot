ad1547e373428a5645b6a9d93ab0cc566e947759
bperraud <74benji@live.fr>
2024-07-22 13:19:28 +0200

    test

--- new_release.sh
+++ new_release.sh
@@ -1,4 +1,11 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <commit-message>"
+    exit 1
+fi
+
+COMMIT_MESSAGE=$1
+
 helm package upylab
 helm repo index --url https://bperraud.github.io/upylab-helm-depot/ .
-git add --all && git commit -m "new release"
+git add --all && git commit -m "$COMMIT_MESSAGE"
 git push
